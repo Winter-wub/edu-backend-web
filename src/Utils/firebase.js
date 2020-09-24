989cd423652ae4039ceb835c277c7278d34d4f01
Winter-wub <wave_onlyone@hotmail.com>
2020-09-24 19:13:21 +0700

    🔧 add firebase analytic

--- src/Utils/firebase.js
+++ src/Utils/firebase.js
@@ -1,10 +1,12 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import "firebase/analytics";
 import config from "../config.json";
 
 export const firebaseApp = firebase.initializeApp(config.firebase);
 export const firestore = firebaseApp.firestore();
+firebase.analytics();
 export const auth = firebaseApp.auth();
 
 if (process.env.NODE_ENV === "development") {
